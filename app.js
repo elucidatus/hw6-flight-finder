455e92c7c027f964757e605d0f887cc79dee59d9
elucidatus <hungnguyen094@gmail.com>
2017-06-09 17:30:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+class App {
+  constructor() {
+    // query the element
+    // make callback functions
+    // make screens: have hide() and show()
+    // "global" variables
+    // bindings
+  }
+}
